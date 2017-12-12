2a0d39e3405fd72ce63bcc192b1d1dd8633ad0d4
Will Smith <w.smith@mepcad.com>
2017-12-12 14:58:49 -0700

    add c hello world

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// A tested and working hello world function in C.
+int main()
+{
+    char* str;
+    str = "Segmentation fault";
+    printf("%s\n", str);
+    return 0;
+}
+
